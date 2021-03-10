34954e2ee6cec2df956105bec0e904c5c5e04604
ksumesh93 <11beeskumar@seecs.edu.pk>
2021-03-09 22:51:11 -0500

    [#9] Adding updated code for Avalon memory bus. Emulating the board memory behavior.

--- fpga/Avalon_bus_RW.vhd
+++ fpga/Avalon_bus_RW.vhd
@@ -30,12 +30,15 @@ USE std.textio.all;
 ENTITY Avalon_RW_bus IS
     GENERIC ( file_path : string := "file_path");
 	PORT (
-		iCLK : IN STD_LOGIC;
+		iCLK_out : OUT STD_LOGIC;
 		iRST_n : IN STD_LOGIC;
 		insert_error : IN STD_LOGIC;
 		drv_status_test_complete : OUT STD_LOGIC;
 
 		command_rw : IN STD_LOGIC;
+		write_addr : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
+		read_addr : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
+		
 		ddr_readdatavalid : OUT STD_LOGIC;
 		ddr_readdata : OUT STD_LOGIC_VECTOR(511 DOWNTO 0);
 		ddr_writedata : IN STD_LOGIC_VECTOR(511 DOWNTO 0);
@@ -46,7 +49,7 @@ END Avalon_RW_bus;
 ARCHITECTURE Behavioral OF Avalon_RW_bus IS
 
 
-constant SCRATCH : string := "C:\Users\sumes\OneDrive\Documents\Xcorr-Design-files\project-code\xcorr\scratch_pad.txt";
+constant SCRATCH : string := "D:\backup\Xcorr-Design-files\project-code\data\scratch_pad.txt";
 
 constant C_DATA_W : integer := 512;
 constant C_CLK_PERIOD : time := 5ns;
@@ -61,7 +64,13 @@ signal eof_b : std_logic;
 signal eof_c : std_logic;
 signal eof_d : std_logic;
 
+signal iCLK : std_logic;
+
 signal number : unsigned(31 downto 0) := x"00000000";
+
+signal read_addr_w : unsigned(4 downto 0);
+signal write_addr_w : unsigned(4 downto 0);
+signal write_en : std_logic;
 
 file fptr_read_ddr : text;
 file fptr_write_ddr : text;
@@ -94,11 +103,20 @@ procedure read_from_file(
         while (not endfile(fptr) and eof = '0') loop
             readline(fptr, file_line);
             if num >= to_integer(line_start) and num <= to_integer(line_end) then
-                start_reading := '1';
                 hread(file_line, var_data);
-                dataline <= var_data;
-                readdatavalid <= '1';
-                wait for 5 ns;
+                if start_reading = '0' then
+                    start_reading := '1';
+                    wait until iCLK = '1';
+                    wait until iCLK = '0';
+                    readdatavalid <= '1';
+                    dataline <= var_data;
+                    wait until iCLK = '1';
+                    wait for 2 ns;
+                else
+                    dataline <= var_data;
+                    readdatavalid <= '1';
+                    wait for 2 ns;
+                end if;
             else
                 readdatavalid <= '0';
                 dataline <= (others => '0');
@@ -115,6 +133,7 @@ procedure write_to_file(
     signal line_end : in unsigned(25 downto 0);
     signal done : out std_logic;
     signal dataline : in std_logic_vector(C_DATA_W-1 downto 0);
+    signal write_begin : out std_logic;
     constant FILE_NAME : in string;
     constant FILE_NAME2 : in string;
     file fptr : text
@@ -129,6 +148,7 @@ procedure write_to_file(
     variable eof : std_logic;
     
     begin
+        write_begin <= '0';
         num := 0;
         eof := '0';
         done <= '0';
@@ -140,13 +160,15 @@ procedure write_to_file(
             if num >= to_integer(line_start) and num <= to_integer(line_end) then
                 readline(fptr_temp, file_line);
                 hwrite(file_line2, dataline);
-                wait for 2.4 ns;
                 writeline(fptr, file_line2);
+                write_begin <= '1';
+                wait for 2ns;
             else
                 readline(fptr_temp, file_line);
                 hread(file_line, var_data);
                 hwrite(file_line2, var_data);
                 writeline(fptr, file_line2);
+                write_begin <= '0';
             end if;
             num := num + 1;
         end loop;
@@ -175,11 +197,12 @@ signal readdata : std_logic_vector(511 downto 0);
 
 BEGIN
 
-    drv_status_test_complete <= operation_complete;
-    address_start <= unsigned(start_address);
-    address_end <= unsigned(end_address);
+
+    
+    
     ddr_readdatavalid <= readdatavalid;
     ddr_readdata <= readdata;
+    
     process
     begin
         if operation_complete = '1' and iRST_n = '1' then
@@ -188,13 +211,63 @@ BEGIN
             readdatavalid <= '0';
         elsif insert_error = '1' and iRST_n = '1' then
             if command_rw = '1' then
-                write_to_file(address_start, address_end, operation_complete, ddr_writedata, file_path, SCRATCH, fptr_write_ddr);
+            
+                write_to_file(address_start, address_end, operation_complete, ddr_writedata, write_en, file_path, SCRATCH, fptr_write_ddr);
             else 
                 read_from_file(address_start, address_end, operation_complete, readdata, readdatavalid, file_path, fptr_read_ddr);
             end if;
+        else
+            operation_complete <= '0';
         end if;
+        address_start <= unsigned(start_address);
+        address_end <= unsigned(end_address);
+        wait for 1ps;
+    end process;
+    
+    process(iCLK,iRST_n)
+    begin
+        if iRST_n = '0' then 
+            drv_status_test_complete <= '0';
+        elsif rising_edge(iCLK) then
+            drv_status_test_complete <= operation_complete;
+        end if;
+    end process;
+    
+    read_addr <= std_logic_vector(read_addr_w);
+    write_addr <= std_logic_vector(write_addr_w);
+    
+    
+    process(iCLK,iRST_n)
+    begin
+        if iRST_n = '0' then 
+            read_addr_w <= (others => '0');
+        elsif falling_edge(iCLK) then
+            if readdatavalid = '1' then
+                read_addr_w <= read_addr_w + 1;
+            end if;
+        end if;
+    end process;
+    
+    process(iCLK,iRST_n)
+    begin
+        if iRST_n = '0' then 
+            write_addr_w <= (others => '0');
+        elsif rising_edge(iCLK) then
+            if write_en = '1' then
+                write_addr_w <= write_addr_w + 1;
+            end if;
+        end if;
+    end process;
+    
+    
+    process
+    begin
+        iCLK <= '1';
+        wait for 1ns;
+        iCLK <= '0';
         wait for 1ns;
     end process;
-
+    
+    iCLK_out <= iCLK;
 
 END Behavioral;
